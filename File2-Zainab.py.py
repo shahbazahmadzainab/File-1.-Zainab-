20d07f23dfb181d75d75a48a9a49252fd47ab0cb
Zainab Shahbaz <shahbazahmadzainab@gmail.com>
2025-07-03 19:17:38 +0500

    Add File2-Zainab.py

---
+++ File2-Zainab.py.py
@@ -0,0 +1,26 @@
+#Print name using the print() function.
+print("Name: Zainab Shahbaz")
+#Create a variable of age and print it.
+print("age: 21")
+#Store your school name in variable and print it.
+print("institute: AI Hub")
+#Add two numbers using variables and print the result.
+a = 5
+b = 10
+add = a + b
+print(add)
+#Store a name in variable and print a greeting message.
+print("Asslam o Alaikum! Name: Zainab")
+#Declare a variale from city and where from live.
+print("I live in Gujranwala")
+#Use multiple variable (name,age,city) and print a full sentence.
+print("Name: Zainab Shahbaz, Age: 21, City: Gujranwala")
+#Perform subtraction between two numbers using variables and the print the answer.
+a = 9
+b = 6
+sub = a - b
+print(sub)
+#Store a motivational qoute in a variable and print it.
+print("Speech is Silver, Silence is Gold")
+#Create a variable for favorite subject and print it.
+print("My favorite subject is 'English'")
