38f19c606ff299a6bbfddc8ae6c61fd83afb4046
Zainab Shahbaz <shahbazahmadzainab@gmail.com>
2025-07-03 20:25:40 +0500

    Add File3-Zainab.py

---
+++ File3-Zainab.py
@@ -0,0 +1,36 @@
+#Declare a variable named age and assign it an integer value 25. 
+age_int: int = 25
+print(type(age_int), "age = ", age_int,)
+#Create a variable called temperature and assign it a float value 98.6.
+temp_float = 98.6
+print(type(temp_float), "temp_float = ", temp_float,)
+#Declare a variable complex_num and assign it a complex number 3 + 4j.
+complex_num = z = 3 + 4j
+print("complex:", z)
+print("real part:", z.real)
+print("imaginary part:", z.imag)
+#Create a variable named is_student and assign it the boolean value True.
+is_student : bool = True #False
+print(type(is_student), "is_student = ", is_student)
+#Declare a variable called has_job and assign it the boolean value False.
+has_job : bool = False #True
+print(type(has_job), "has_job = ", has_job)
+#Create a variable greeting and assign it the string 'Hello' using single string.
+greeting : str = 'Hello' #string eith single quote (')
+print(type(greeting), "greeting = ", greeting)
+#Declare a variable named city and assign it the string "New York" using double quotes.
+city : str = "New York" #strings with double quotes ("")
+print(type(city), "city = ", city)
+#Create a variable qoute and assign it the string 'Life is short' using triple single qoutes.
+qoute : str = '''Life is short''' #strings with triple qoutes (''')
+print(type(qoute), "qoute = ", qoute)
+#Declare a variable called poem and assign it a multi-line string using triple qoutes with the text: "Roses are red, Violets are blue.
+poem : str = """Roses are red, Violets are blue""" #multi-line strings with triple qoutes (""")
+print(type(poem), "poem = ", poem)
+#**Create 3 variables: score as an integer with value 95  height as a float with value 5.9  name as a string with value "Alice".
+score : int = 95
+height : float = 5.9
+name : str = "Alice"
+print(type(score), "score = ", score)
+print(type(height), "height = ", height)
+print(type(name), "name = ", name)
